0cbd4da91624841dd2f722af713033df6ae6e01e
Mrunali <mrunaliurade26@gmail.com>
2023-04-13 22:58:29 +0530

    example of static block

---
+++ Block.java
@@ -0,0 +1,18 @@
+
+public class Block {
+ 
+	static int a1;
+	
+	
+	static{
+		a1=14;
+		System.out.println("This is static block");
+		System.out.println(a1);
+	}
+	
+	
+public static void main(String[] args) {
+	int a=45;
+	System.out.println(a);
+}
+}
