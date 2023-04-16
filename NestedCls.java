3b989eed7a5f1ad876fedf46cb29f9c1f1c9dae4
Mrunali <mrunaliurade26@gmail.com>
2023-04-16 23:17:38 +0530

    example of static Nested Class

---
+++ NestedCls.java
@@ -0,0 +1,35 @@
+
+
+class Demo
+{
+	 static class MyDemo
+	{
+		void myfun()
+		{
+			System.out.println("this is in mydemo cls");
+		}
+	}
+	
+	void fun()
+	{
+		System.out.println("this is in demo");
+	}
+}
+
+public class NestedCls {
+	public static void main(String[] args) {
+		
+	
+	Demo d= new Demo();
+	
+	Demo.MyDemo d1= new Demo.MyDemo();
+	
+	 d.fun();
+	 d1.myfun();
+    
+    
+    
+	}
+}
+
+
